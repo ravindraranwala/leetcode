ec4b0ed57a47722ff71a4b67c4cc1ff4dda7418f
Ranwala <Ravindra.Ranwala@pearson.com>
2019-09-09 19:40:44 +0530

    code was refactored after going through the analysis.

--- src/main/java/com/coding/challenge/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/coding/challenge/LongestSubstringWithoutRepeatingCharacters.java
@@ -20,8 +20,8 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
 	public static int lengthOfLongestSubstring(String s) {
 		Map<Character, Integer> charDistributionMap = new HashMap<>();
-		int maxLength = 0, windowStart = 0, substrLen = 0;
-		for (int i = 0, n = s.length(); i < n; i++) {
+		int maxLength = 0, substrLen = 0;
+		for (int i = 0, n = s.length(), windowStart = 0; i < n; i++) {
 			final int previousIdx = charDistributionMap.getOrDefault(s.charAt(i), -1);
 			// Checking for a duplicate.
 			if (previousIdx != -1 && previousIdx >= windowStart) {
