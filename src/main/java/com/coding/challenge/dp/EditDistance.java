f329940f60bed72aa575cd84898d6b0d899307a6
Ravindra <ravindra.ranwala@pearson.com>
2020-05-16 21:31:58 +0530

    Improving asymptotic space requirement.

--- src/main/java/com/coding/challenge/dp/EditDistance.java
+++ src/main/java/com/coding/challenge/dp/EditDistance.java
@@ -30,4 +30,28 @@ public class EditDistance {
 		}
 		return c[m][n];
 	}
+
+	private static int editDistanceTwoRows(String x, String y) {
+		final int m = x.length();
+		final int n = y.length();
+		final int[][] c = new int[2][n + 1];
+
+		for (int j = 0; j <= n; j++)
+			c[0][j] = j;
+
+		for (int i = 1; i <= m; i++) {
+			final int newI = i % 2;
+			c[newI][0] = i;
+			final int prevI = newI % 2 == 0 ? newI + 1 : newI - 1;
+			for (int j = 1; j <= n; j++) {
+				// Copy task
+				if (x.charAt(i - 1) == y.charAt(j - 1))
+					c[newI][j] = c[prevI][j - 1];
+				else
+					// Replace, Insert and Delete tasks are considered here.
+					c[newI][j] = Math.min(Math.min(c[prevI][j], c[newI][j - 1]), c[prevI][j - 1]) + 1;
+			}
+		}
+		return c[m % 2][n];
+	}
 }
