4c052b91d1e46b06e9d682580942e20e8ddf585f
Ravindra Ranwala <ravindraranwala@Ravindras-MacBook-Pro.local>
2021-06-24 15:24:12 +0530

    Palindrome Linked List exercise.

---
+++ src/main/java/com/coding/challenge/linked/list/PalindromeLinkedList.java
@@ -0,0 +1,75 @@
+package com.coding.challenge.linked.list;
+
+class PalindromeLinkedList {
+
+	public PalindromeLinkedList() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final ListNode head = new ListNode(1);
+		final ListNode n1 = new ListNode(1, null);
+		head.next = n1;
+//		final ListNode n2 = new ListNode(2);
+//		n1.next = n2;
+//		final ListNode n3 = new ListNode(1);
+//		n2.next = n3;
+		System.out.println(isPalindrome(head));
+	}
+
+	static boolean isPalindrome(ListNode head) {
+		if (head == null)
+			throw new IllegalArgumentException("Empty list.");
+		int counter = 0;
+		for (ListNode current = head; current != null; current = current.next)
+			counter++;
+
+		if (counter == 1)
+			return true;
+		ListNode current = head;
+		ListNode next = head.next;
+		current.next = null;
+		for (int i = 1; i < counter / 2; i++) {
+			final ListNode previous = current;
+			current = next;
+			next = current.next;
+			current.next = previous;
+		}
+
+		// current pointer sets to the middle of the linkedlist now.
+		ListNode lowerRight = next;
+		if (counter % 2 != 0)
+			lowerRight = lowerRight.next;
+		boolean isPalind = true;
+
+		while (current != null && lowerRight != null) {
+			if (isPalind && current.val != lowerRight.val)
+				isPalind = false;
+			lowerRight = lowerRight.next;
+			// Fix the pointer rearrangement again.
+			final ListNode previous = current.next;
+			current.next = next;
+			next = current;
+			current = previous;
+		}
+		return isPalind;
+	}
+
+	/**
+	 * Definition for singly-linked list.
+	 */
+	static class ListNode {
+		final int val;
+		ListNode next;
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+}
