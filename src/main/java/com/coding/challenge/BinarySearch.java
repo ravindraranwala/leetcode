cd30e670dc9b3db85da8933c44e4e9954ea00561
Ravindra <ravindra.ranwala@pearson.com>
2021-07-01 15:11:39 +0530

    Binary search implementation.

---
+++ src/main/java/com/coding/challenge/BinarySearch.java
@@ -0,0 +1,44 @@
+package com.coding.challenge;
+
+public class BinarySearch {
+
+	BinarySearch() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] nums = { -1, 0, 3, 5, 9, 12 };
+		final int target = 9;
+		final int idx = search(nums, target);
+		System.out.println(idx);
+	}
+
+	/**
+	 * Given an array of integers nums which is sorted in ascending order, and an
+	 * integer target, this function searches target in nums. If target exists, then
+	 * return its index. Otherwise, return -1.
+	 * 
+	 * @param nums   an array of integers
+	 * @param target integer target
+	 * @return index of the target if it exists in the array. Otherwise, return -1
+	 */
+	public static int search(int[] nums, int target) {
+		return binarySearch(nums, target, 0, nums.length - 1);
+	}
+
+	static int binarySearch(int[] nums, int target, int low, int high) {
+		// base case of the recursion
+		if (low == high) {
+			if (nums[low] == target)
+				return low;
+			else
+				return -1;
+		}
+
+		final int mid = (low + high) / 2;
+		if (target <= nums[mid])
+			return binarySearch(nums, target, low, mid);
+		else
+			return binarySearch(nums, target, mid + 1, high);
+	}
+}
