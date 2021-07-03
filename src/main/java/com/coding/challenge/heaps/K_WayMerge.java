667b9f23886461af426eeeb57e83cf3c919fdd1c
Ravindra Ranwala <ravindraranwala@Ravindras-MacBook-Pro.local>
2021-07-03 21:45:45 +0530

    addressing empty lists scenarios.

--- src/main/java/com/coding/challenge/heaps/K_WayMerge.java
+++ src/main/java/com/coding/challenge/heaps/K_WayMerge.java
@@ -10,7 +10,33 @@ public class K_WayMerge {
 	}
 
 	public static void main(String[] args) {
+		// Testcase one
+		final ListNode<Integer> listOneHead = new ListNode<>(1);
+		final ListNode<Integer> listOneNode2 = new ListNode<>(4);
+		listOneHead.next = listOneNode2;
+		final ListNode<Integer> listOneNode3 = new ListNode<>(5);
+		listOneNode2.next = listOneNode3;
 
+		final ListNode<Integer> listTwoHead = new ListNode<>(1);
+		final ListNode<Integer> listTwoNode2 = new ListNode<>(3);
+		listTwoHead.next = listTwoNode2;
+		final ListNode<Integer> listTwoNode3 = new ListNode<>(4);
+		listTwoNode2.next = listTwoNode3;
+
+		final ListNode<Integer> listThreeHead = new ListNode<>(2);
+		final ListNode<Integer> listThreeNode2 = new ListNode<>(6);
+		listThreeHead.next = listThreeNode2;
+
+		final ListNode<Integer> mergedList = kWayMerge(listOneHead, listTwoHead, listThreeHead);
+		System.out.println(mergedList);
+
+		// Testcase two
+		final ListNode<Integer> mergedList2 = kWayMerge();
+		System.out.println(mergedList2);
+
+		// Testcase three
+		final ListNode<Integer> mergedList3 = kWayMerge(new ListNode<>(null));
+		System.out.println(mergedList3);
 	}
 
 	public static <T> ListNode<T> kWayMerge(Comparator<? super T> cmp, ListNode<T>... sortedLists) {
@@ -18,7 +44,8 @@ public class K_WayMerge {
 		final Queue<ListNode<T>> queue = PriorityQueue.of(listNodeCmp);
 		for (ListNode<T> a : sortedLists)
 			// Adding the first element of each list to the heap.
-			queue.insert(a);
+			if (a.val != null)
+				queue.insert(a);
 
 		final ListNode<T> dummyHead = new ListNode<>(null);
 		ListNode<T> current = dummyHead;
@@ -30,7 +57,7 @@ public class K_WayMerge {
 			if (nextNode != null)
 				queue.insert(nextNode);
 		}
-		return dummyHead.next;
+		return dummyHead.next != null ? dummyHead.next : dummyHead;
 	}
 
 	public static <T extends Comparable<? super T>> ListNode<T> kWayMerge(ListNode<T>... sortedLists) {
