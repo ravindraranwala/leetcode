844c96c46340b306f4b4ffd7dec2c19c4865e08e
Ravindra <ravindra.ranwala@pearson.com>
2021-07-07 07:00:04 +0530

    Longest Increasing Subsequence dp-solution.

---
+++ src/main/java/com/coding/challenge/dp/LIS.java
@@ -0,0 +1,64 @@
+package com.coding.challenge.dp;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class LIS {
+	private LIS() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] nums = { 0, 1, 0, 3, 2, 3 };
+		int lisLength = lengthOfLIS(nums);
+		System.out.println(String.format("Length of LIS: %d", lisLength));
+	}
+
+	public static int lengthOfLIS(int[] nums) {
+		/*
+		 * All we have to do is, sort the array while keeping only the distinct elements
+		 * and then get the LCS between the original and distinct sorted
+		 * sequences.Sorting takes (n log n) time and LCS takes n^2. So, overall it
+		 * takes n^2 time.
+		 */
+		final int[] sortedDistinctNums = sortedDistinctNums(nums);
+		int[][] lcsLength = lcsLength(nums, sortedDistinctNums);
+		return lcsLength[nums.length][sortedDistinctNums.length];
+	}
+
+	static int[] sortedDistinctNums(int[] nums) {
+		final Set<Integer> uniqueNums = new HashSet<>();
+		for (int n : nums)
+			uniqueNums.add(n);
+		final int[] sortedDistinctNums = new int[uniqueNums.size()];
+		int i = 0;
+		for (int n : uniqueNums)
+			sortedDistinctNums[i++] = n;
+		Arrays.sort(sortedDistinctNums);
+		return sortedDistinctNums;
+	}
+
+	static int[][] lcsLength(int[] x, int[] y) {
+		final int m = x.length;
+		final int n = y.length;
+		final int[][] c = new int[m + 1][n + 1];
+
+		for (int i = 1; i <= m; i++)
+			c[i][0] = 0;
+		for (int j = 0; j <= n; j++)
+			c[0][j] = 0;
+
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (x[i - 1] == y[j - 1])
+					c[i][j] = c[i - 1][j - 1] + 1;
+				else if (c[i - 1][j] >= c[i][j - 1])
+					c[i][j] = c[i - 1][j];
+				else
+					c[i][j] = c[i][j - 1];
+			}
+		}
+		return c;
+	}
+}
