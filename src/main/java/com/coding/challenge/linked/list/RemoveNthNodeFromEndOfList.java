40234c6c3cd96410f272736d623fec1835783d96
Ravindra <ravindra.ranwala@pearson.com>
2021-07-29 10:04:20 +0530

    Remove Nth Node From End of List.

---
+++ src/main/java/com/coding/challenge/linked/list/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,47 @@
+package com.coding.challenge.linked.list;
+
+import com.coding.challenge.ListNode;
+
+class RemoveNthNodeFromEndOfList {
+
+	RemoveNthNodeFromEndOfList() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final ListNode<Integer> one = new ListNode<>(1);
+		final ListNode<Integer> two = new ListNode<>(2);
+		final ListNode<Integer> three = new ListNode<>(3);
+		final ListNode<Integer> four = new ListNode<>(4);
+		final ListNode<Integer> five = new ListNode<>(5);
+		one.next = two;
+		two.next = three;
+		three.next = four;
+		four.next = five;
+
+		ListNode<Integer> head = removeNthFromEnd(one, 2);
+		System.out.println(head);
+
+		head = removeNthFromEnd(new ListNode<>(1), 1);
+		System.out.println(head);
+		
+		final ListNode<Integer> one2 = new ListNode<>(1);
+		one2.next = new ListNode<>(2);
+		head = removeNthFromEnd(one2, 1);
+		System.out.println(head);
+	}
+
+	static <T> ListNode<T> removeNthFromEnd(ListNode<T> head, int n) {
+		ListNode<T> nPrev = head;
+		int counter = 0;
+		for (ListNode<T> curr = head; curr != null; curr = curr.next) {
+			counter++;
+			if (counter > n + 1)
+				nPrev = nPrev.next;
+		}
+		if (n == counter)
+			return head.next;
+		nPrev.next = nPrev.next.next;
+		return head;
+	}
+}
