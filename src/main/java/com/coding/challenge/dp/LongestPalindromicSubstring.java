e9449830885fe9cb5dce1a8b0c26f418a104b2f3
Ravindra <ravindra.ranwala@pearson.com>
2021-08-29 15:40:53 +0530

    Longest Palindromic Substring code refactoring completed.

--- src/main/java/com/coding/challenge/dp/LongestPalindromicSubstring.java
+++ src/main/java/com/coding/challenge/dp/LongestPalindromicSubstring.java
@@ -6,31 +6,33 @@ class LongestPalindromicSubstring {
 	}
 
 	public static void main(String[] args) {
-		final String s = "aaaa";
+		final String s = "babadbabad";
 		final String p = longestPalindrome(s);
 		System.out.println(p);
 	}
 
 	public static String longestPalindrome(String s) {
-		if (s.isEmpty())
-			return s;
-
 		final int n = s.length();
 		final boolean[][] p = new boolean[n][n];
-		for (int i = 0; i < n; i++)
-			p[i][i] = true;
-
 		int l = 1;
 		int start = 0;
-		for (int len = 2; len <= n; len++) {
+		for (int i = 0; i < n; i++)
+			p[i][i] = true;
+		for (int i = 1; i < n; i++) {
+			p[i - 1][i] = s.charAt(i - 1) == s.charAt(i);
+			if (p[i - 1][i]) {
+				l = 2;
+				start = i - 1;
+			}
+		}
+		for (int len = 3; len <= n; len++) {
 			for (int i = 0; i < n - len + 1; i++) {
 				final int j = i + len - 1;
-				if (s.charAt(i) == s.charAt(j) && (len == 2 || p[i + 1][j - 1])) {
-					p[i][j] = true;
+				p[i][j] = s.charAt(i) == s.charAt(j) && p[i + 1][j - 1];
+				if (p[i][j]) {
 					start = i;
 					l = len;
-				} else
-					p[i][j] = false;
+				}
 			}
 		}
 		return s.substring(start, start + l);
