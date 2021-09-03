44370a1446bc63877eaf31d3f825b8180594c8b2
Ravindra <ravindra.ranwala@pearson.com>
2021-09-03 06:55:08 +0530

    235. Lowest Common Ancestor of a Binary Search Tree code refactoring.

--- src/main/java/com/coding/challenge/bst/LowestCommonAncestor.java
+++ src/main/java/com/coding/challenge/bst/LowestCommonAncestor.java
@@ -37,10 +37,10 @@ class LowestCommonAncestor {
 	}
 
 	static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		if (root.val < p.val && root.val < q.val)
-			return lowestCommonAncestor(root.right, p, q);
-		else if (root.val > p.val && root.val > q.val)
+		if (p.val < root.val && q.val < root.val)
 			return lowestCommonAncestor(root.left, p, q);
+		else if (root.val < p.val && root.val < q.val)
+			return lowestCommonAncestor(root.right, p, q);
 		else
 			return root; // base case of the recursion.
 	}
