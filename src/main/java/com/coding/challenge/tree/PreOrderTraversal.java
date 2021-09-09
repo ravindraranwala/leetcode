e8d406197207d403c95238aec092fe3b10690e83
Ravindra <ravindra.ranwala@pearson.com>
2021-09-09 07:19:31 +0530

    144. Binary Tree Preorder Traversal using Morris method.

--- src/main/java/com/coding/challenge/tree/PreOrderTraversal.java
+++ src/main/java/com/coding/challenge/tree/PreOrderTraversal.java
@@ -1,8 +1,6 @@
 package com.coding.challenge.tree;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 import com.coding.challenge.bst.TreeNode;
