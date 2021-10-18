656451e8e6f2244a6e09e0dae5827603c8e3fdde
Ravindra <ravindra.ranwala@pearson.com>
2021-10-18 20:14:18 +0530

    198. House Robber completed.

---
+++ src/main/java/com/coding/challenge/dp/HouseRobber.java
@@ -0,0 +1,48 @@
+package com.coding.challenge.dp;
+
+class HouseRobber {
+
+	HouseRobber() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] nums1 = { 1, 2, 3, 1 };
+		int w = rob(nums1);
+		System.out.println(w);
+
+		final int[] nums2 = { 2, 7, 9, 3, 1 };
+		w = rob(nums2);
+		System.out.println(w);
+
+		final int[] nums3 = { 2, 7, 9, 8, 1 };
+		w = rob(nums3);
+		System.out.println(w);
+
+		final int[] nums4 = { 2, 1, 1, 2 };
+		w = rob(nums4);
+		System.out.println(w);
+
+		final int[] nums5 = { 1, 3, 1, 3, 100 };
+		w = rob(nums5);
+		System.out.println(w);
+
+		final int[] nums6 = { 6, 3, 10, 8, 2, 10, 3, 5, 10, 5, 3 };
+		w = rob(nums6);
+		System.out.println(w);
+	}
+
+	static int rob(int[] nums) {
+		int p1 = nums[0];
+		int p2 = p1;
+		final int n = nums.length;
+		if (1 < n)
+			p2 = Math.max(p1, nums[1]);
+		for (int i = 2; i < n; i++) {
+			final int tmp = p1 + nums[i];
+			p1 = p2;
+			p2 = Math.max(tmp, p2);
+		}
+		return p2;
+	}
+}
