793933fffba7bc6f922508e00103f5e77e7f261d
Ravindra <ravindra.ranwala@pearson.com>
2021-10-29 09:32:39 +0530

    509. Fibonacci Number code refactoring.

--- src/main/java/com/coding/challenge/dp/FibonacciNumber.java
+++ src/main/java/com/coding/challenge/dp/FibonacciNumber.java
@@ -1,25 +1,27 @@
 package com.coding.challenge.dp;
 
-public class FibonacciNumber {
+class FibonacciNumber {
 
 	private FibonacciNumber() {
 		throw new AssertionError();
 	}
 
 	public static void main(String[] args) {
-		final int n = 6;
+		final int n = 0;
 		System.out.println(String.format("Fibonacci %d: %d", n, fib(n)));
 	}
 
 	public static int fib(int n) {
 		// trivial case.
-		if (n < 2)
-			return n;
-		final int[] fibSeries = new int[n + 1];
-		fibSeries[0] = 0;
-		fibSeries[1] = 1;
-		for (int i = 2; i <= n; i++)
-			fibSeries[i] = fibSeries[i - 1] + fibSeries[i - 2];
-		return fibSeries[n];
+		if (n == 0)
+			return 0;
+		int p = 0;
+		int c = 1;
+		for (int i = 2; i <= n; i++) {
+			final int tmp = p + c;
+			p = c;
+			c = tmp;
+		}
+		return c;
 	}
 }
