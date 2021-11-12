e7db09e6cd5071661a5136774dc26b84cb929dd8
Ravindra <ravindra.ranwala@pearson.com>
2021-11-12 15:37:02 +0530

    713. Subarray Product Less Than K.

---
+++ src/main/java/com/coding/challenge/dp/SubArrPrdLessThanK.java
@@ -0,0 +1,50 @@
+package com.coding.challenge.dp;
+
+class SubArrPrdLessThanK {
+
+	SubArrPrdLessThanK() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] nums1 = { 10, 5, 2, 6 };
+		final int k1 = 100;
+		int c = numSubarrayProductLessThanK(nums1, k1);
+		System.out.println(c);
+
+		final int[] nums2 = { 1, 2, 3 };
+		final int k2 = 0;
+		c = numSubarrayProductLessThanK(nums2, k2);
+		System.out.println(c);
+
+		final int[] nums3 = { 10, 5, 2, 10 };
+		c = numSubarrayProductLessThanK(nums3, k1);
+		System.out.println(c);
+
+		final int[] nums4 = { 10, 5, 2, 75 };
+		c = numSubarrayProductLessThanK(nums4, k1);
+		System.out.println(c);
+		
+		final int[] nums5 = { 10, 5, 2, 100 };
+		c = numSubarrayProductLessThanK(nums5, k1);
+		System.out.println(c);
+	}
+
+	static int numSubarrayProductLessThanK(int[] nums, int k) {
+		int s = 0;
+		int c = 0;
+		final int n = nums.length;
+		int p = 1;
+		for (int i = 0; i < n; i++) {
+			p = p * nums[i];
+			while (k <= p && s < i) {
+				p = p / nums[s];
+				s = s + 1;
+			}
+			if (p < k)
+				c = c + i - s + 1;
+		}
+		return c;
+	}
+
+}
