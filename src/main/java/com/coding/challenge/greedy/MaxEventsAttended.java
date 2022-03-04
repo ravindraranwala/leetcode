5eaa5d57914e3faae15dca6610aa908286db678b
Ravindra Ranwala <ravindraranwala@gmail.com>
2022-03-04 21:00:24 +0800

    1353. Maximum Number of Events That Can Be Attended accepted solution.

--- src/main/java/com/coding/challenge/greedy/MaxEventsAttended.java
+++ src/main/java/com/coding/challenge/greedy/MaxEventsAttended.java
@@ -1,6 +1,11 @@
 package com.coding.challenge.greedy;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import org.junit.Assert;
 
 class MaxEventsAttended {
 	MaxEventsAttended() {
@@ -11,10 +16,46 @@ class MaxEventsAttended {
 		final int[][] events = { { 27, 27 }, { 8, 10 }, { 9, 11 }, { 20, 21 }, { 25, 29 }, { 17, 20 }, { 12, 12 },
 				{ 12, 12 }, { 10, 14 }, { 7, 7 }, { 6, 10 }, { 7, 7 }, { 4, 8 }, { 30, 31 }, { 23, 25 }, { 4, 6 },
 				{ 17, 17 }, { 13, 14 }, { 6, 9 }, { 13, 14 } };
-		Arrays.sort(events,
-				(a, b) -> Integer.compare(a[0], b[0]) == 0 ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
-		System.out.println(Arrays.deepToString(events));
-		System.out.println(events.length);
+		int m = maxEvents(events);
+		Assert.assertEquals(18, m);
+		System.out.println(m);
+
+		final int[][] events2 = { { 1, 2 }, { 1, 2 }, { 3, 3 }, { 1, 5 }, { 1, 5 } };
+		m = maxEvents(events2);
+		Assert.assertEquals(5, m);
+		System.out.println(m);
+
+		final int[][] events3 = { { 1, 5 }, { 1, 5 }, { 1, 5 }, { 2, 3 }, { 2, 3 } };
+		m = maxEvents(events3);
+		System.out.println(m);
+		Assert.assertEquals(5, m);
 	}
 
+	static int maxEvents(int[][] events) {
+		final int n = events.length;
+		final Queue<Integer> h = new PriorityQueue<>(Integer::compare);
+		Arrays.sort(events, Comparator.<int[]>comparingInt(e -> e[0]).thenComparingInt(e -> e[1]));
+		int c = 0;
+		int s = events[0][0];
+		int e = 0;
+		int p = 0;
+		for (int[] evnt : events)
+			e = Math.max(e, evnt[1]);
+		for (int d = s; d <= e; d++) {
+			while (!h.isEmpty() && h.peek() < d)
+				h.remove();
+
+			while (p < n && events[p][0] == d) {
+				h.add(events[p][1]);
+				p = p + 1;
+			}
+
+			// then try to attend the event.
+			if (!h.isEmpty()) {
+				h.remove();
+				c = c + 1;
+			}
+		}
+		return c;
+	}
 }
