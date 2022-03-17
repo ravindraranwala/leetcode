aed5be5622ec4c9b26c2319a26edfcddb850111b
Ravindra Ranwala <ravindraranwala@gmail.com>
2022-03-17 22:54:52 +0800

    2054-TwoBestNon-OverlappingEvents 2054. Two Best Non-Overlapping Events.

--- src/main/java/com/coding/challenge/dp/TwoBestNonOverlappingEvents.java
+++ src/main/java/com/coding/challenge/dp/TwoBestNonOverlappingEvents.java
@@ -1,10 +1,11 @@
 package com.coding.challenge.dp;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
-import com.coding.challenge.BinarySearch;
+import org.junit.Assert;
 
-public class TwoBestNonOverlappingEvents {
+class TwoBestNonOverlappingEvents {
 	TwoBestNonOverlappingEvents() {
 		throw new AssertionError();
 	}
@@ -13,35 +14,38 @@ public class TwoBestNonOverlappingEvents {
 		final int[][] events1 = { { 1, 3, 2 }, { 4, 5, 2 }, { 2, 4, 3 } };
 		int maxVal = maxTwoEvents(events1);
 		System.out.println(maxVal);
+		Assert.assertEquals(4, maxVal);
 
 		final int[][] events2 = { { 1, 3, 2 }, { 4, 5, 2 }, { 1, 5, 5 } };
 		maxVal = maxTwoEvents(events2);
 		System.out.println(maxVal);
+		Assert.assertEquals(5, maxVal);
 
 		final int[][] events3 = { { 1, 5, 3 }, { 1, 5, 1 }, { 6, 6, 5 } };
-		maxVal =  maxTwoEvents(events3);
+		maxVal = maxTwoEvents(events3);
+		System.out.println(maxVal);
+		Assert.assertEquals(8, maxVal);
+
+		final int[][] events4 = { { 66, 97, 90 }, { 98, 98, 68 }, { 38, 49, 63 }, { 91, 100, 42 }, { 92, 100, 22 },
+				{ 1, 77, 50 }, { 64, 72, 97 } };
+		maxVal = maxTwoEvents(events4);
+		Assert.assertEquals(165, maxVal);
 		System.out.println(maxVal);
 	}
 
 	static int maxTwoEvents(int[][] events) {
-		final int n = events.length;
-		Arrays.sort(events, (a, b) -> Integer.compare(a[1], b[1]));
-		final int[][] p = new int[n + 1][3];
-		// trivial case of our recursion where the solution is known.
-		for (int i = 0; i <= n; i++)
-			p[i][0] = 0;
-		for (int j = 1; j <= 2; j++)
-			p[0][j] = 0;
-
-		for (int i = 1; i <= n; i++) {
-			final int c = BinarySearch.predecessor(events, events[i - 1][0], e -> e[1]);
-			for (int j = 1; j <= 2; j++) {
-				int v = 0;
-				if (c >= 0)
-					v = p[c + 1][j - 1];
-				p[i][j] = Math.max(v + events[i - 1][2], p[i - 1][j]);
+		Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+		final PriorityQueue<int[]> h = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+		int maxVal = 0;
+		int ans = 0;
+		for (int[] e : events) {
+			while (!h.isEmpty() && h.peek()[1] < e[0]) {
+				final int[] nonOverlappingEvnt = h.remove();
+				maxVal = Math.max(maxVal, nonOverlappingEvnt[2]);
 			}
+			ans = Math.max(ans, maxVal + e[2]);
+			h.add(e);
 		}
-		return p[n][2];
+		return ans;
 	}
 }
