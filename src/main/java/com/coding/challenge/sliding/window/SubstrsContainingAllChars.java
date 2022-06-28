0b1dfbc142350f923ab8974cb0e5891f04636e0a
Ravindra Ranwala <ravindraranwala@gmail.com>
2022-06-28 18:58:36 +0800

    1358. Number of Substrings Containing All Three Characters.

---
+++ src/main/java/com/coding/challenge/sliding/window/SubstrsContainingAllChars.java
@@ -0,0 +1,55 @@
+package com.coding.challenge.sliding.window;
+
+class SubstrsContainingAllChars {
+	SubstrsContainingAllChars() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		int n = numberOfSubstrings("abcabc");
+		assert n == 10;
+
+		n = numberOfSubstrings("aaacb");
+		assert n == 3;
+
+		n = numberOfSubstrings("abc");
+		assert n == 1;
+
+		n = numberOfSubstrings("aab");
+		assert n == 0;
+
+		n = numberOfSubstrings("ababbbc");
+		assert n == 3;
+	}
+
+	static int numberOfSubstrings(String s) {
+		final int n = s.length();
+		final int[] p = { -1, -1, -1 };
+		int i = 0;
+		int j = 0;
+		int c = 0;
+
+		while (i < n) {
+			if (p[0] >= i && p[1] >= i && p[2] >= i) {
+				c = c + n - j + 1;
+				i = i + 1;
+			} else if (j < n) {
+				final char ch = s.charAt(j);
+				p[encode(ch)] = j;
+				j = j + 1;
+			} else
+				i = i + 1;
+		}
+		return c;
+	}
+
+	static int encode(char ch) {
+		if (ch == 'a')
+			return 0;
+		if (ch == 'b')
+			return 1;
+		if (ch == 'c')
+			return 2;
+		throw new IllegalArgumentException();
+	}
+}
