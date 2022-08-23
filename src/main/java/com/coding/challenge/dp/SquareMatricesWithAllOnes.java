7b413331d4fa4acc33722f731e4c01f5a88665e0
Ravindra Ranwala <ravindraranwala@gmail.com>
2022-08-23 09:46:15 +0800

    code refactoring.

--- src/main/java/com/coding/challenge/dp/SquareMatricesWithAllOnes.java
+++ src/main/java/com/coding/challenge/dp/SquareMatricesWithAllOnes.java
@@ -7,31 +7,28 @@ class SquareMatricesWithAllOnes {
 
 	public static void main(String[] args) {
 		final int[][] m1 = { { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 1, 1, 1 } };
-		int cnt = countSquaresV2(m1);
+		int cnt = countSquares(m1);
 		assert cnt == 15;
 
 		final int[][] m2 = { { 1, 0, 1 }, { 1, 1, 0 }, { 1, 1, 0 } };
-		cnt = countSquaresV2(m2);
+		cnt = countSquares(m2);
 		assert cnt == 7;
 	}
 
 	static int countSquares(int[][] matrix) {
 		final int m = matrix.length;
 		final int n = matrix[0].length;
-		final int[][] s = new int[m][n];
+		final int[][] s = new int[m + 1][n + 1];
 		int c = 0;
-		for (int i = 0; i < m; i++) {
-			s[i][0] = matrix[i][0];
-			c = c + s[i][0];
-		}
-		for (int j = 1; j < n; j++) {
-			s[0][j] = matrix[0][j];
-			c = c + s[0][j];
-		}
+		for (int i = 0; i < m; i++)
+			s[i][0] = 0;
 
-		for (int i = 1; i < m; i++) {
-			for (int j = 1; j < n; j++) {
-				if (matrix[i][j] == 0)
+		for (int j = 1; j < n; j++)
+			s[0][j] = 0;
+
+		for (int i = 1; i < m + 1; i++) {
+			for (int j = 1; j < n + 1; j++) {
+				if (matrix[i - 1][j - 1] == 0)
 					s[i][j] = 0;
 				else
 					s[i][j] = Math.min(s[i - 1][j], Math.min(s[i - 1][j - 1], s[i][j - 1])) + 1;
