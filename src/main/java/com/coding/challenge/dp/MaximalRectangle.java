5852d315a591a65934c1dba6a92250f1a4810e17
Ravindra Ranwala <ravindraranwala@gmail.com>
2022-09-12 07:46:06 +0800

    85. Maximal Rectangle optimal solution using a stack.

--- src/main/java/com/coding/challenge/dp/MaximalRectangle.java
+++ src/main/java/com/coding/challenge/dp/MaximalRectangle.java
@@ -1,6 +1,10 @@
 package com.coding.challenge.dp;
 
+import com.coding.challenge.stack.LargestRectangleInHistogram;
+
 class MaximalRectangle {
+	private static final char ZERO = '0';
+
 	MaximalRectangle() {
 		throw new AssertionError();
 	}
@@ -8,21 +12,21 @@ class MaximalRectangle {
 	public static void main(String[] args) {
 		final char[][] m1 = { { '1', '0', '1', '0', '0' }, { '1', '0', '1', '1', '1' }, { '1', '1', '1', '1', '1' },
 				{ '1', '0', '0', '1', '0' } };
-		int ans = maximalRectangle(m1);
+		int ans = maximalRectangleV2(m1);
 		assert ans == 6;
 
 		final char[][] m2 = { { '0' } };
-		ans = maximalRectangle(m2);
+		ans = maximalRectangleV2(m2);
 		assert ans == 0;
 
 		final char[][] m3 = { { '1' } };
-		ans = maximalRectangle(m3);
+		ans = maximalRectangleV2(m3);
 		assert ans == 1;
 
 		final char[][] m4 = { { '0', '0', '1', '0' }, { '0', '0', '1', '0' }, { '0', '0', '1', '0' },
 
 				{ '0', '0', '1', '1' }, { '0', '1', '1', '1' }, { '0', '1', '1', '1' }, { '1', '1', '1', '1' } };
-		ans = maximalRectangle(m4);
+		ans = maximalRectangleV2(m4);
 		assert ans == 9;
 	}
 
@@ -56,5 +60,29 @@ class MaximalRectangle {
 			}
 		}
 		return ans;
+	}
+
+	static int maximalRectangleV2(char[][] matrix) {
+		final int r = matrix.length;
+		final int c = matrix[0].length;
+		final int[] h = new int[r];
+
+		// deal with the first column
+		for (int k = 0; k < r; k++)
+			h[k] = matrix[k][0] - ZERO;
+
+		int l = LargestRectangleInHistogram.largestRectangleArea(h);
+		for (int i = 1; i < c; i++) {
+			// compute the bar heights of the histogram first.
+			for (int j = 0; j < r; j++) {
+				if (matrix[j][i] == ZERO)
+					h[j] = 0;
+				else
+					h[j] = h[j] + 1;
+			}
+			// then find the largest rectangle in histogram.
+			l = Math.max(l, LargestRectangleInHistogram.largestRectangleArea(h));
+		}
+		return l;
 	}
 }
