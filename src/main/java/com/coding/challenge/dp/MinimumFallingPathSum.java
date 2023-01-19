b17c56704f7c2af1334ebaea5688ec2f9a10fc92
Ravindra Ranwala <ravindraranwala@gmail.com>
2023-01-19 09:25:35 +0800

    931. Minimum Falling Path Sum accepted solution.

---
+++ src/main/java/com/coding/challenge/dp/MinimumFallingPathSum.java
@@ -0,0 +1,41 @@
+package com.coding.challenge.dp;
+
+class MinimumFallingPathSum {
+	MinimumFallingPathSum() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[][] matrixOne = { { 2, 1, 3 }, { 6, 5, 4 }, { 7, 8, 9 } };
+		assert minFallingPathSum(matrixOne) == 13;
+
+		final int[][] matrixTwo = { { -19, 57 }, { -40, -5 } };
+		assert minFallingPathSum(matrixTwo) == -59;
+	}
+
+	static int minFallingPathSum(int[][] matrix) {
+		final int n = matrix.length;
+		final int[][] f = new int[n][n + 2];
+
+		for (int i = 0; i < n; i++) {
+			f[i][0] = Integer.MAX_VALUE;
+			f[i][n + 1] = Integer.MAX_VALUE;
+		}
+
+		// trivial case of the recursion.
+		for (int j = 1; j <= n; j++)
+			f[n - 1][j] = matrix[n - 1][j - 1];
+
+		// recursive step.
+		for (int i = n - 2; i >= 0; i--)
+			for (int j = 1; j <= n; j++)
+				f[i][j] = Math.min(Math.min(f[i + 1][j - 1], f[i + 1][j]), f[i + 1][j + 1]) + matrix[i][j - 1];
+
+		// constructing an optimal solution.
+		int minSum = Integer.MAX_VALUE;
+		for (int j = 1; j <= n; j++)
+			minSum = Math.min(minSum, f[0][j]);
+
+		return minSum;
+	}
+}
