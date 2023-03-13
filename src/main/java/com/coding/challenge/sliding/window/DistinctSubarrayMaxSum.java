c5c125f43a4b83946e0f48420cbaeda2ddfc3251
Ravindra Ranwala <ravindraranwala@gmail.com>
2023-03-13 08:13:53 +0800

    2461. Maximum Sum of Distinct Subarrays With Length K accepted solution.

---
+++ src/main/java/com/coding/challenge/sliding/window/DistinctSubarrayMaxSum.java
@@ -0,0 +1,41 @@
+package com.coding.challenge.sliding.window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class DistinctSubarrayMaxSum {
+	DistinctSubarrayMaxSum() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] nums = { 1, 5, 4, 2, 9, 9, 9 };
+		System.out.println(maximumSubarraySum(nums, 3));
+
+		final int[] numsTwo = { 4, 4, 4 };
+		System.out.println(maximumSubarraySum(numsTwo, 3));
+	}
+
+	static long maximumSubarraySum(int[] nums, int k) {
+		// Fixed size window problem.
+		final int n = nums.length;
+		final Map<Integer, Integer> numFreq = new HashMap<>();
+		long s = 0;
+		long runningSum = 0;
+
+		for (int i = 0; i < n; i++) {
+			numFreq.merge(nums[i], 1, Integer::sum);
+			runningSum = runningSum + nums[i];
+			if (i >= k - 1) {
+				if (numFreq.size() == k)
+					s = Math.max(s, runningSum);
+				final int low = i - k + 1;
+				numFreq.put(nums[low], numFreq.get(nums[low]) - 1);
+				if (numFreq.get(nums[low]) == 0)
+					numFreq.remove(nums[low]);
+				runningSum = runningSum - nums[low];
+			}
+		}
+		return s;
+	}
+}
