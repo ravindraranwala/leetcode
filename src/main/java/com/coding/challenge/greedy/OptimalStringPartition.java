64bf1425c909118c80855fe9d5585db7988bface
Ravindra Ranwala <ravindraranwala@gmail.com>
2023-04-13 09:36:36 +0800

    2405 Optimal Partition of String.

--- src/main/java/com/coding/challenge/greedy/OptimalStringPartition.java
+++ src/main/java/com/coding/challenge/greedy/OptimalStringPartition.java
@@ -16,14 +16,14 @@ class OptimalStringPartition {
 	static int partitionString(String s) {
 		final int n = s.length();
 		int cnt = 0;
-		int[] window = new int[26];
+		int[] freq = new int[26];
 
 		for (int j = 0; j < n; j++) {
-			if (window[s.charAt(j) - FIRST_LETTER] > 0) {
+			if (freq[s.charAt(j) - FIRST_LETTER] > 0) {
 				cnt = cnt + 1;
-				window = new int[26];
+				freq = new int[26];
 			}
-			window[s.charAt(j) - FIRST_LETTER] = window[s.charAt(j) - FIRST_LETTER] + 1;
+			freq[s.charAt(j) - FIRST_LETTER] = freq[s.charAt(j) - FIRST_LETTER] + 1;
 		}
 		// make sure that you count the remaining window when you terminate the loop.
 		return cnt + 1;
