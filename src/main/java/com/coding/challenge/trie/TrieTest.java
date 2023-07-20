0eb7c4ee23e1a64626b0d4991b6762333ada79ac
Ravindra Ranwala <ravindraranwala@gmail.com>
2023-07-21 06:53:27 +0800

    208. Implement Trie (Prefix Tree)

--- src/main/java/com/coding/challenge/trie/TrieTest.java
+++ src/main/java/com/coding/challenge/trie/TrieTest.java
@@ -1,23 +1,25 @@
 package com.coding.challenge.trie;
 
-public class TrieTest {
+class TrieTest {
+	TrieTest() {
+		throw new AssertionError();
+	}
 
 	public static void main(String[] args) {
 		// test case one
-		Trie trie = new Trie();
-		trie.insert("apple");
-		System.out.println(trie.search("apple")); // return True
-		System.out.println(trie.search("app")); // return False
-		System.out.println(trie.startsWith("app")); // return True
-		trie.insert("app");
-		System.out.println(trie.search("app")); // return True
+		final Trie trieOne = new Trie();
+		trieOne.insert("apple");
+		assert trieOne.search("apple"); // return True
+		assert !trieOne.search("app"); // return False
+		assert trieOne.startsWith("app"); // return True
+		trieOne.insert("app");
+		assert trieOne.search("app"); // return True
 
-		System.out.println("=======================");
 		// test case two.
 		final Trie trieTwo = new Trie();
 		trieTwo.insert("a");
-		System.out.println(trieTwo.search("a"));
-		System.out.println(trieTwo.startsWith("a"));
+		assert trieTwo.search("a"); // return True
+		assert trieTwo.startsWith("a");// return True
 	}
 
 }
