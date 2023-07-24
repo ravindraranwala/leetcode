9c5a7f3a39ab1d811b6643edf7953d9b3deba2b1
Ravindra Ranwala <ravindraranwala@gmail.com>
2023-07-25 07:50:49 +0800

    2610. Convert an Array Into a 2D Array With Conditions.

---
+++ src/main/java/com/coding/challenge/hash/table/ConvertArrayInto2DArrayWithConditions.java
@@ -0,0 +1,43 @@
+package com.coding.challenge.hash.table;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+class ConvertArrayInto2DArrayWithConditions {
+	ConvertArrayInto2DArrayWithConditions() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] numsOne = { 1, 3, 4, 1, 2, 3, 1 };
+		System.out.println(findMatrix(numsOne));
+
+		final int[] numsTwo = { 1, 2, 3, 4 };
+		System.out.println(findMatrix(numsTwo));
+	}
+
+	static List<List<Integer>> findMatrix(int[] nums) {
+		final Map<Integer, Integer> f = new HashMap<>();
+		for (int v : nums)
+			f.merge(v, 1, Integer::sum);
+
+		final List<List<Integer>> m = new ArrayList<>();
+		while (!f.isEmpty()) {
+			final List<Integer> l = new ArrayList<>();
+			final Iterator<Entry<Integer, Integer>> it = f.entrySet().iterator();
+			while (it.hasNext()) {
+				final Entry<Integer, Integer> e = it.next();
+				l.add(e.getKey());
+				e.setValue(e.getValue() - 1);
+				if (e.getValue() == 0)
+					it.remove();
+			}
+			m.add(l);
+		}
+		return m;
+	}
+}
