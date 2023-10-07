25fc8bb40d848d1cc93b2f65df99da6b2f2b5c30
Ravindra Ranwala <ravindraranwala@gmail.com>
2023-10-07 10:28:53 +0800

    2233. Maximum Product After K Increments.

---
+++ src/main/java/com/coding/challenge/greedy/MaximumProductAfterKIncrements.java
@@ -0,0 +1,33 @@
+package com.coding.challenge.greedy;
+
+import java.util.PriorityQueue;
+
+class MaximumProductAfterKIncrements {
+	MaximumProductAfterKIncrements() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] numsOne = { 0, 4 };
+		assert maximumProduct(numsOne, 5) == 20;
+
+		final int[] numsTwo = { 6, 3, 3, 2 };
+		assert maximumProduct(numsTwo, 2) == 216;
+	}
+
+	static int maximumProduct(int[] nums, int k) {
+		// min heap.
+		final PriorityQueue<Integer> h = new PriorityQueue<>();
+		for (int val : nums)
+			h.add(val);
+
+		for (int i = 0; i < k; i++)
+			h.add(h.remove() + 1);
+
+		int p = 1;
+		while (!h.isEmpty())
+			p = (int) (p * (long) h.remove() % 1000000007);
+
+		return p;
+	}
+}
