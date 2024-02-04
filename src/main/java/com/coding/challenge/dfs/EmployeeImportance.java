8d5ff45cdb8a3abaa07214dfbf3658c9829c69c6
Ravindra Ranwala <ravindraranwala@gmail.com>
2024-02-04 11:32:22 +0800

    690. Employee Importance.

---
+++ src/main/java/com/coding/challenge/dfs/EmployeeImportance.java
@@ -0,0 +1,35 @@
+package com.coding.challenge.dfs;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class EmployeeImportance {
+	EmployeeImportance() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+	static int getImportance(List<Employee> employees, int id) {
+		final Map<Integer, Employee> empById = new HashMap<>();
+		for (Employee emp : employees)
+			empById.put(emp.id, emp);
+		return visitHierarchy(empById.get(id), empById);
+	}
+
+	private static int visitHierarchy(Employee emp, Map<Integer, Employee> empById) {
+		int importance = emp.importance;
+		for (int subId : emp.subordinates)
+			importance = importance + visitHierarchy(empById.get(subId), empById);
+		return importance;
+	}
+
+	class Employee {
+		public int id;
+		public int importance;
+		public List<Integer> subordinates;
+	};
+}
