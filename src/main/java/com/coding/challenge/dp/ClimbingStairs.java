6489d26d2c344e747a6f6c6979f1831910ef98eb
Ravindra Ranwala <ravindraranwala@gmail.com>
2024-02-05 17:26:53 +0800

    70. Climbing Stairs.

---
+++ src/main/java/com/coding/challenge/dp/ClimbingStairs.java
@@ -0,0 +1,21 @@
+package com.coding.challenge.dp;
+
+class ClimbingStairs {
+	ClimbingStairs() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		assert climbStairs(4) == 5;
+	}
+
+	static int climbStairs(int n) {
+		int w = 0;
+		for (int i = 1, prev = 1, beforePrev = 0; i <= n; i++) {
+			w = prev + beforePrev;
+			beforePrev = prev;
+			prev = w;
+		}
+		return w;
+	}
+}
