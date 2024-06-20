55f384c7bccf5026f6d815982e207fa43589b488
Ravindra Ranwala <ravindraranwala@gmail.com>
2024-06-20 09:34:44 +0800

    826. Most Profit Assigning Work.

--- src/main/java/com/coding/challenge/greedy/MostProfitAssigningWork.java
+++ src/main/java/com/coding/challenge/greedy/MostProfitAssigningWork.java
@@ -8,38 +8,46 @@ class MostProfitAssigningWork {
 	}
 
 	public static void main(String[] args) {
-		final int[] difficultyOne = { 2, 4, 6, 8, 10 };
-		final int[] profitOne = { 10, 20, 30, 40, 50 };
-		final int[] workerOne = { 4, 5, 6, 7 };
-		assert maxProfitAssignment(difficultyOne, profitOne, workerOne) == 100;
+		final int[] difficulty1 = { 2, 4, 6, 8, 10 };
+		final int[] profit1 = { 10, 20, 30, 40, 50 };
+		final int[] worker1 = { 4, 5, 6, 7 };
+		assert maxProfitAssignment(difficulty1, profit1, worker1) == 100;
 
-		final int[] difficultyTwo = { 85, 47, 57 };
-		final int[] profitTwo = { 24, 66, 99 };
-		final int[] workerTwo = { 40, 25, 25 };
-		assert maxProfitAssignment(difficultyTwo, profitTwo, workerTwo) == 0;
-
-		final int[] difficultyThree = { 13, 37, 58 };
-		final int[] profitThree = { 4, 90, 96 };
-		final int[] workerThree = { 34, 73, 45 };
-		assert maxProfitAssignment(difficultyThree, profitThree, workerThree) == 190;
+		final int[] difficulty2 = { 85, 47, 57 };
+		final int[] profit2 = { 24, 66, 99 };
+		final int[] worker2 = { 40, 25, 25 };
+		assert maxProfitAssignment(difficulty2, profit2, worker2) == 0;
 	}
 
 	static int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+		final int m = worker.length;
 		final int n = difficulty.length;
-		final int[][] jobs = new int[n][2];
+		final Job[] jobs = new Job[n];
 		for (int i = 0; i < n; i++)
-			jobs[i] = new int[] { difficulty[i], profit[i] };
+			jobs[i] = new Job(difficulty[i], profit[i]);
 
-		Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
+		Arrays.sort(jobs, (a, b) -> Integer.compare(a.d, b.d));
+
 		Arrays.sort(worker);
 		int p = 0;
-		for (int i = 0, j = 0, maxProfit = 0, m = worker.length; i < m; i++) {
-			while (j < n && jobs[j][0] <= worker[i]) {
-				maxProfit = Math.max(maxProfit, jobs[j][1]);
+		for (int i = 0, j = 0, maxProfit = 0; i < m; i++) {
+			while (j < n && jobs[j].d <= worker[i]) {
+				maxProfit = Math.max(maxProfit, jobs[j].p);
 				j = j + 1;
 			}
+
 			p = p + maxProfit;
 		}
 		return p;
+	}
+
+	private static class Job {
+		private final int d;
+		private final int p;
+
+		Job(int d, int p) {
+			this.d = d;
+			this.p = p;
+		}
 	}
 }
