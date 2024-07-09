fa74b3f16e5665947e58919c585925be182ed458
Ravindra Ranwala <ravindraranwala@gmail.com>
2024-07-09 21:58:08 +0800

    1701. Average Waiting Time.

--- src/main/java/com/coding/challenge/simulation/AverageWaitingTime.java
+++ src/main/java/com/coding/challenge/simulation/AverageWaitingTime.java
@@ -6,20 +6,20 @@ class AverageWaitingTime {
 	}
 
 	public static void main(String[] args) {
-		final int[][] customers1 = { { 1, 2 }, { 2, 5 }, { 4, 3 } };
-		assert averageWaitingTime(customers1) == 5.0;
+		final int[][] customersOne = { { 1, 2 }, { 2, 5 }, { 4, 3 } };
+		assert averageWaitingTime(customersOne) == 5.0;
 
-		final int[][] customers2 = { { 5, 2 }, { 5, 4 }, { 10, 3 }, { 20, 1 } };
-		assert averageWaitingTime(customers2) == 3.25;
+		final int[][] customersTwo = { { 5, 2 }, { 5, 4 }, { 10, 3 }, { 20, 1 } };
+		assert averageWaitingTime(customersTwo) == 3.25;
 	}
 
 	static double averageWaitingTime(int[][] customers) {
+		long waitingTime = 0;
 		final int n = customers.length;
-		long totalWaitingTime = 0;
-		for (int i = 0, t = customers[0][0]; i < n; i++) {
-			t = Math.max(t, customers[i][0]) + customers[i][1];
-			totalWaitingTime = totalWaitingTime + t - customers[i][0];
+		for (int i = 0, elapsedTime = 0; i < n; i++) {
+			elapsedTime = Math.max(elapsedTime, customers[i][0]) + customers[i][1];
+			waitingTime = waitingTime + elapsedTime - customers[i][0];
 		}
-		return totalWaitingTime * 1.0 / n;
+		return waitingTime * 1.0 / n;
 	}
 }
