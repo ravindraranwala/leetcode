92ff76b415cfaa4a0a070181bd7bb52774c923bf
Ravindra Ranwala <ravindraranwala@gmail.com>
2024-07-24 07:39:44 +0800

    2418. Sort the People.

--- src/main/java/com/coding/challenge/radix/sort/SortThePeople.java
+++ src/main/java/com/coding/challenge/radix/sort/SortThePeople.java
@@ -1,6 +1,8 @@
 package com.coding.challenge.radix.sort;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class SortThePeople {
 	SortThePeople() {
@@ -21,54 +23,14 @@ class SortThePeople {
 
 	static String[] sortPeople(String[] names, int[] heights) {
 		final int n = names.length;
-		final Person[] people = new Person[n];
+		final Map<Integer, String> heightToName = new HashMap<>();
 		for (int i = 0; i < n; i++)
-			people[i] = new Person(heights[i], names[i]);
+			heightToName.put(heights[i], names[i]);
 
-		final Person[] sortedPeople = radixSort(people, 6);
+		final int[] sortedHeights = RadixSort.radixSort(heights, 6);
 		final String[] sortedNames = new String[n];
 		for (int i = 0; i < n; i++)
-			sortedNames[n - 1 - i] = sortedPeople[i].name;
+			sortedNames[n - 1 - i] = heightToName.get(sortedHeights[i]);
 		return sortedNames;
-	}
-
-	private static Person[] radixSort(Person[] a, int d) {
-		Person[] b = a;
-		for (int i = 0; i < d; i++)
-			b = countingSort(b, 9, i);
-		return b;
-	}
-
-	private static Person[] countingSort(Person[] a, int k, int d) {
-		final int n = a.length;
-		final Person[] b = new Person[n];
-		final int[] c = new int[k + 1];
-
-		final int denominator = (int) Math.pow(10, d);
-		for (int j = 0; j < n; j++) {
-			final int digit = (a[j].height / denominator) % (k + 1);
-			c[digit] = c[digit] + 1;
-		}
-
-		for (int i = 1; i <= k; i++)
-			c[i] = c[i] + c[i - 1];
-
-		for (int j = n - 1; j >= 0; j--) {
-			final int digit = (a[j].height / denominator) % (k + 1);
-			b[c[digit] - 1] = a[j];
-			c[digit] = c[digit] - 1;
-		}
-		return b;
-	}
-
-	private static class Person {
-		final int height;
-		final String name;
-
-		public Person(int height, String name) {
-			this.height = height;
-			this.name = name;
-		}
-
 	}
 }
