51263d6afe62cc9ed748883b62b61d79e81de401
Ravindra Ranwala <ravindraranwala@gmail.com>
2024-10-06 09:52:52 +0800

    567. Permutation in String.

--- src/main/java/com/coding/challenge/sliding/window/PermutationInString.java
+++ src/main/java/com/coding/challenge/sliding/window/PermutationInString.java
@@ -1,44 +1,40 @@
 package com.coding.challenge.sliding.window;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class PermutationInString {
+	private static final char FIRST_LETTER = 'a';
+
 	PermutationInString() {
 		throw new AssertionError();
 	}
 
 	public static void main(String[] args) {
 		assert checkInclusion("ab", "eidbaooo");
-		
 		assert !checkInclusion("ab", "eidboaoo");
 	}
 
 	static boolean checkInclusion(String s1, String s2) {
 		final int m = s1.length();
-		final Map<Character, Integer> sf = new HashMap<>();
+		final int[] f1 = new int[26];
 		for (char ch : s1.toCharArray())
-			sf.merge(ch, 1, Integer::sum);
+			f1[ch - FIRST_LETTER] = f1[ch - FIRST_LETTER] + 1;
 
+		final int[] f = new int[26];
 		final int n = s2.length();
-		final Map<Character, Integer> wf = new HashMap<>();
-		int c = 0;
-		boolean ans = false;
 
 		for (int i = 0; i < n; i++) {
-			final char endCh = s2.charAt(i);
-			wf.merge(endCh, 1, Integer::sum);
-			if (wf.get(endCh).equals(sf.get(endCh)))
-				c = c + 1;
+			f[s2.charAt(i) - FIRST_LETTER] = f[s2.charAt(i) - FIRST_LETTER] + 1;
+			// Fixed size window.
 			if (i >= m - 1) {
-				ans = ans || c == sf.size();
-				// sliding the window by discarding the starting character
-				final char startCh = s2.charAt(i - m + 1);
-				if (wf.get(startCh).equals(sf.get(startCh)))
-					c = c - 1;
-				wf.put(startCh, wf.get(startCh) - 1);
+				boolean p = true;
+				for (int k = 0; k < 26; k++)
+					p = p && f[k] == f1[k];
+
+				if (p)
+					return true;
+
+				f[s2.charAt(i - m + 1) - FIRST_LETTER] = f[s2.charAt(i - m + 1) - FIRST_LETTER] - 1;
 			}
 		}
-		return ans;
+		return false;
 	}
 }
