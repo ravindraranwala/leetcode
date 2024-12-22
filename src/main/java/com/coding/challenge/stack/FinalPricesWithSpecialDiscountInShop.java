9abafc67a0bc335abea98439ca40ce2b86c4636f
Ravindra Ranwala <ravindraranwala@gmail.com>
2024-12-22 09:27:27 +0800

    1475. Final Prices With a Special Discount in a Shop.

---
+++ src/main/java/com/coding/challenge/stack/FinalPricesWithSpecialDiscountInShop.java
@@ -0,0 +1,38 @@
+package com.coding.challenge.stack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class FinalPricesWithSpecialDiscountInShop {
+	FinalPricesWithSpecialDiscountInShop() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] prices1 = { 8, 4, 6, 2, 3 };
+		final int[] a1 = { 4, 2, 4, 2, 3 };
+		assert Arrays.equals(a1, finalPrices(prices1));
+
+		final int[] prices2 = { 1, 2, 3, 4, 5 };
+		final int[] a2 = { 1, 2, 3, 4, 5 };
+		assert Arrays.equals(a2, finalPrices(prices2));
+
+		final int[] prices3 = { 10, 1, 1, 6 };
+		final int[] a3 = { 9, 0, 1, 6 };
+		assert Arrays.equals(a3, finalPrices(prices3));
+	}
+
+	static int[] finalPrices(int[] prices) {
+		final int n = prices.length;
+		final Deque<Integer> s = new ArrayDeque<>();
+		for (int i = 0; i < n; i++) {
+			while (!s.isEmpty() && prices[s.peek()] >= prices[i]) {
+				final int j = s.pop();
+				prices[j] = prices[j] - prices[i];
+			}
+			s.push(i);
+		}
+		return prices;
+	}
+}
