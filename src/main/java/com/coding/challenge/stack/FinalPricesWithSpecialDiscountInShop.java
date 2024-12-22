86f97c51bf9b438c8c2130ecb1f537a788243c06
Ravindra Ranwala <ravindraranwala@gmail.com>
2024-12-22 09:31:08 +0800

    1475. Final Prices With a Special Discount in a Shop.

--- src/main/java/com/coding/challenge/stack/FinalPricesWithSpecialDiscountInShop.java
+++ src/main/java/com/coding/challenge/stack/FinalPricesWithSpecialDiscountInShop.java
@@ -11,16 +11,16 @@ class FinalPricesWithSpecialDiscountInShop {
 
 	public static void main(String[] args) {
 		final int[] prices1 = { 8, 4, 6, 2, 3 };
-		final int[] a1 = { 4, 2, 4, 2, 3 };
-		assert Arrays.equals(a1, finalPrices(prices1));
+		final int[] d1 = { 4, 2, 4, 2, 3 };
+		assert Arrays.equals(d1, finalPrices(prices1));
 
 		final int[] prices2 = { 1, 2, 3, 4, 5 };
-		final int[] a2 = { 1, 2, 3, 4, 5 };
-		assert Arrays.equals(a2, finalPrices(prices2));
+		final int[] d2 = { 1, 2, 3, 4, 5 };
+		assert Arrays.equals(d2, finalPrices(prices2));
 
 		final int[] prices3 = { 10, 1, 1, 6 };
-		final int[] a3 = { 9, 0, 1, 6 };
-		assert Arrays.equals(a3, finalPrices(prices3));
+		final int[] d3 = { 9, 0, 1, 6 };
+		assert Arrays.equals(d3, finalPrices(prices3));
 	}
 
 	static int[] finalPrices(int[] prices) {
