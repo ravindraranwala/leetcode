8d643efb60b47bf25b0de21b232b8bb928734f5f
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-01-11 08:02:19 +0800

    916. Word Subsets.

--- src/main/java/com/coding/challenge/hash/table/WordSubsets.java
+++ src/main/java/com/coding/challenge/hash/table/WordSubsets.java
@@ -1,7 +1,6 @@
 package com.coding.challenge.hash.table;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class WordSubsets {
@@ -14,36 +13,38 @@ class WordSubsets {
 	public static void main(String[] args) {
 		final String[] words1 = { "amazon", "apple", "facebook", "google", "leetcode" };
 		final String[] words2 = { "e", "o" };
-		assert Arrays.asList("facebook", "google", "leetcode").equals(wordSubsets(words1, words2));
+		System.out.println(wordSubsets(words1, words2));
 
 		final String[] words3 = { "amazon", "apple", "facebook", "google", "leetcode" };
 		final String[] words4 = { "l", "e" };
-		assert Arrays.asList("apple", "google", "leetcode").equals(wordSubsets(words3, words4));
+		System.out.println(wordSubsets(words3, words4));
 	}
 
 	static List<String> wordSubsets(String[] words1, String[] words2) {
-		final int[] highestFreq = new int[26];
+		final int[] maxFreq = new int[26];
 		for (String word : words2) {
-			final int[] currFreq = new int[26];
-			for (char ch : word.toCharArray())
-				currFreq[ch - FIRST_LETTER] = currFreq[ch - FIRST_LETTER] + 1;
-
+			final int[] currFreq = charFreq(word);
 			for (int i = 0; i < 26; i++)
-				highestFreq[i] = Math.max(highestFreq[i], currFreq[i]);
+				maxFreq[i] = Math.max(maxFreq[i], currFreq[i]);
 		}
 
-		final List<String> universalStrs = new ArrayList<>();
+		final List<String> l = new ArrayList<>();
 		for (String word : words1) {
-			final int[] letterFreq = new int[26];
-			for (char ch : word.toCharArray())
-				letterFreq[ch - FIRST_LETTER] = letterFreq[ch - FIRST_LETTER] + 1;
-			boolean isUniversal = true;
-			for (int i = 0; i < 26; i++)
-				isUniversal = isUniversal && letterFreq[i] >= highestFreq[i];
+			final int[] f = charFreq(word);
+			int i = 0;
+			while (i < 26 && f[i] >= maxFreq[i])
+				i = i + 1;
 
-			if (isUniversal)
-				universalStrs.add(word);
+			if (i == 26)
+				l.add(word);
 		}
-		return universalStrs;
+		return l;
+	}
+
+	private static int[] charFreq(String word) {
+		final int[] currFreq = new int[26];
+		for (char ch : word.toCharArray())
+			currFreq[ch - FIRST_LETTER] = currFreq[ch - FIRST_LETTER] + 1;
+		return currFreq;
 	}
 }
