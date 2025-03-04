7309c1871cb588a073d9cf6785fced2e7bf20f3a
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-03-04 08:08:36 +0800

    2161. Partition Array According to Given Pivot.

--- src/main/java/com/coding/challenge/two/pointer/PartitionArrayAccordingToGivenPivot.java
+++ src/main/java/com/coding/challenge/two/pointer/PartitionArrayAccordingToGivenPivot.java
@@ -8,33 +8,40 @@ class PartitionArrayAccordingToGivenPivot {
 	}
 
 	public static void main(String[] args) {
-		final int[] numsOne = { 9, 12, 5, 10, 14, 3, 10 };
-		final int[] expectedOne = { 9, 5, 3, 10, 10, 12, 14 };
-		assert Arrays.equals(expectedOne, pivotArray(numsOne, 10));
+		final int[] nums1 = { 9, 12, 5, 10, 14, 3, 10 };
+		final int[] a1 = { 9, 5, 3, 10, 10, 12, 14 };
+		assert Arrays.equals(a1, pivotArray(nums1, 10));
 
-		final int[] numsTwo = { -3, 4, 3, 2 };
-		final int[] expectedTwo = { -3, 2, 4, 3 };
-		assert Arrays.equals(expectedTwo, pivotArray(numsTwo, 2));
+		final int[] nums2 = { -3, 4, 3, 2 };
+		final int[] a2 = { -3, 2, 4, 3 };
+		assert Arrays.equals(a2, pivotArray(nums2, 2));
 	}
 
 	static int[] pivotArray(int[] nums, int pivot) {
 		final int n = nums.length;
 		final int[] a = new int[n];
 		int i = 0;
-		int j = n - 1;
 		for (int k = 0; k < n; k++) {
-			if (nums[k] < pivot) {
-				a[i] = nums[k];
+			final int val = nums[k];
+			if (val < pivot) {
+				a[i] = val;
 				i = i + 1;
 			}
-			if (nums[n - k - 1] > pivot) {
-				a[j] = nums[n - k - 1];
+		}
+
+		int j = n - 1;
+		for (int k = n - 1; k >= 0; k--) {
+			final int val = nums[k];
+			if (val > pivot) {
+				a[j] = val;
 				j = j - 1;
 			}
 		}
 
-		for (int l = i; l <= j; l++)
-			a[l] = pivot;
+		while (i <= j) {
+			a[i] = pivot;
+			i = i + 1;
+		}
 
 		return a;
 	}
