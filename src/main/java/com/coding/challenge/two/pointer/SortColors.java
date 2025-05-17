edf49ea3f23c2ff7ce4ea616361881b32717fdf5
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-05-18 07:40:08 +0800

    75. Sort Colors.

--- src/main/java/com/coding/challenge/two/pointer/SortColors.java
+++ src/main/java/com/coding/challenge/two/pointer/SortColors.java
@@ -17,30 +17,41 @@ class SortColors {
 		sortColors(nums2);
 		final int[] expected2 = { 0, 1, 2 };
 		assert Arrays.equals(expected2, nums2);
+
+		final int[] nums3 = { 1, 2, 1, 2, 1, 1, 1 };
+		sortColors(nums3);
+		final int[] expected3 = { 1, 1, 1, 1, 1, 2, 2 };
+		assert Arrays.equals(expected3, nums3);
+
+		final int[] nums4 = { 2, 1, 2, 2, 2, 1, 2, 2, 2, 2 };
+		sortColors(nums4);
+		final int[] expected4 = { 1, 1, 2, 2, 2, 2, 2, 2, 2, 2 };
+		assert Arrays.equals(expected4, nums4);
+
+		final int[] nums5 = { 1, 1, 1, 1, 1, 1, 1 };
+		sortColors(nums5);
+		final int[] expected5 = { 1, 1, 1, 1, 1, 1, 1 };
+		assert Arrays.equals(expected5, nums5);
 	}
 
-	/*
-	 * An extension of the Quick Sort Partition algorithm. In Quick Sort, we have
-	 * just three partitions whereas here we have four partitions.
-	 */
 	static void sortColors(int[] nums) {
 		final int n = nums.length;
-		for (int k = 0, i = -1, j = -1; k < n; k++) {
-			if (nums[k] == 0) {
+		for (int k = 0, i = 0, j = 0; k < n; k++) {
+			if (nums[k] == 1) {
+				exchange(nums, k, j);
+				j = j + 1;
+			} else if (nums[k] == 0) {
+				exchange(nums, k, j);
+				exchange(nums, i, j);
 				i = i + 1;
 				j = j + 1;
-				exchange(nums, j, k);
-				exchange(nums, i, j);
-			} else if (nums[k] == 1) {
-				j = j + 1;
-				exchange(nums, j, k);
 			}
 		}
 	}
 
-	private static void exchange(int[] a, int i, int j) {
-		final int tmp = a[i];
-		a[i] = a[j];
-		a[j] = tmp;
+	private static void exchange(int[] nums, int i, int j) {
+		final int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
 	}
 }
