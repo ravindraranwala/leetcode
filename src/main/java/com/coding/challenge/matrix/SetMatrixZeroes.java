d44f648fc9e11dcefa251df862facc2e097c519b
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-05-22 17:55:16 +0800

    73. Set Matrix Zeroes.

--- src/main/java/com/coding/challenge/matrix/SetMatrixZeroes.java
+++ src/main/java/com/coding/challenge/matrix/SetMatrixZeroes.java
@@ -10,55 +10,53 @@ class SetMatrixZeroes {
 	public static void main(String[] args) {
 		final int[][] matrix1 = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
 		setZeroes(matrix1);
-		final int[][] m1 = { { 1, 0, 1 }, { 0, 0, 0 }, { 1, 0, 1 } };
-		assert Arrays.deepEquals(matrix1, m1);
+		final int[][] expected1 = { { 1, 0, 1 }, { 0, 0, 0 }, { 1, 0, 1 } };
+		assert Arrays.deepEquals(expected1, matrix1);
 
 		final int[][] matrix2 = { { 0, 1, 2, 0 }, { 3, 4, 5, 2 }, { 1, 3, 1, 5 } };
 		setZeroes(matrix2);
-		final int[][] m2 = { { 0, 0, 0, 0 }, { 0, 4, 5, 0 }, { 0, 3, 1, 0 } };
-		assert Arrays.deepEquals(matrix2, m2);
+		final int[][] expected2 = {{0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}};
+		assert Arrays.deepEquals(expected2, matrix2);
 	}
 
 	static void setZeroes(int[][] matrix) {
 		final int m = matrix.length;
 		final int n = matrix[0].length;
-		int firstRowZeros = 0;
+		boolean setFirstRow = false;
 		for (int j = 0; j < n; j++)
 			if (matrix[0][j] == 0)
-				firstRowZeros = firstRowZeros + 1;
+				setFirstRow = true;
 
-		int firstColZeros = 0;
-		for (int i = 0; i < m; i++)
-			if (matrix[i][0] == 0)
-				firstColZeros = firstColZeros + 1;
-
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == 0) {
-					matrix[i][0] = 0;
+		/*
+		 * go through each cell, if there's a zero, then update the corresponding entry
+		 * in the first row by setting it to zero.
+		 */
+		for (int i = 1; i < m; i++)
+			for (int j = 0; j < n; j++)
+				if (matrix[i][j] == 0)
 					matrix[0][j] = 0;
-				}
-			}
-		}
 
+		// starting from row 1, set the row to zero if required.
 		for (int i = 1; i < m; i++) {
-			if (matrix[i][0] == 0)
-				for (int j = 1; j < n; j++)
+			boolean setRow = false;
+			for (int j = 0; j < n; j++)
+				if (matrix[i][j] == 0)
+					setRow = true;
+
+			if (setRow)
+				for (int j = 0; j < n; j++)
 					matrix[i][j] = 0;
 		}
-
-		for (int j = 1; j < n; j++) {
-			if (matrix[0][j] == 0)
-				for (int i = 1; i < m; i++)
+		// now process the columns and set it to zero if required.
+		// just bring the zero columns down starting from the first row.
+		for (int i = 1; i < m; i++)
+			for (int j = 0; j < n; j++)
+				if (matrix[0][j] == 0)
 					matrix[i][j] = 0;
-		}
 
-		if (firstRowZeros > 0)
+		// now process the first row.
+		if (setFirstRow)
 			for (int j = 0; j < n; j++)
 				matrix[0][j] = 0;
-
-		if (firstColZeros > 0)
-			for (int i = 0; i < m; i++)
-				matrix[i][0] = 0;
 	}
 }
