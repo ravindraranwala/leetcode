d0dcc2c7cb09e0657128e6337e2070423df5f23e
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-08-02 08:25:28 +0800

    118. Pascal's Triangle.

--- src/main/java/com/coding/challenge/array/PascalsTriangle.java
+++ src/main/java/com/coding/challenge/array/PascalsTriangle.java
@@ -22,14 +22,11 @@ class PascalsTriangle {
 
 		// init.
 		final List<List<Integer>> ans = new ArrayList<>();
-		for (int r = 1; r <= numRows; r++)
-			ans.add(new ArrayList<>());
-
-		ans.get(0).add(1);
-		ans.get(1).add(1);
-		ans.get(1).add(1);
+		ans.add(Arrays.asList(1));
+		ans.add(Arrays.asList(1, 1));
 
 		for (int i = 2; i < numRows; i++) {
+			ans.add(new ArrayList<>());
 			ans.get(i).add(1);
 			for (int j = 1; j < i; j++)
 				ans.get(i).add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
