de4bcb86d9dd2c634202b4226585d071d7fd5d36
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-08-02 08:17:50 +0800

    118. Pascal's Triangle.

---
+++ src/main/java/com/coding/challenge/array/PascalsTriangle.java
@@ -0,0 +1,40 @@
+package com.coding.challenge.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class PascalsTriangle {
+	PascalsTriangle() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		assert Arrays.asList(Arrays.asList(1), Arrays.asList(1, 1), Arrays.asList(1, 2, 1), Arrays.asList(1, 3, 3, 1),
+				Arrays.asList(1, 4, 6, 4, 1)).equals(generate(5));
+		assert Arrays.asList(Arrays.asList(1)).equals(generate(1));
+		assert Arrays.asList(Arrays.asList(1), Arrays.asList(1, 1)).equals(generate(2));
+	}
+
+	static List<List<Integer>> generate(int numRows) {
+		if (numRows == 1)
+			return Arrays.asList(Arrays.asList(1));
+
+		// init.
+		final List<List<Integer>> ans = new ArrayList<>();
+		for (int r = 1; r <= numRows; r++)
+			ans.add(new ArrayList<>());
+
+		ans.get(0).add(1);
+		ans.get(1).add(1);
+		ans.get(1).add(1);
+
+		for (int i = 2; i < numRows; i++) {
+			ans.get(i).add(1);
+			for (int j = 1; j < i; j++)
+				ans.get(i).add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+			ans.get(i).add(1);
+		}
+		return ans;
+	}
+}
