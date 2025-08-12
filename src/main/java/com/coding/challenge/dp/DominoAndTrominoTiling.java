2d7f50a5e6e20d46b8c2edc892ea4d38194fc490
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-08-12 08:02:33 +0800

    790. Domino and Tromino Tiling.

--- src/main/java/com/coding/challenge/dp/DominoAndTrominoTiling.java
+++ src/main/java/com/coding/challenge/dp/DominoAndTrominoTiling.java
@@ -16,20 +16,21 @@ class DominoAndTrominoTiling {
 		assert numTilingsMath(1) == 1;
 		assert numTilingsMath(3) == 5;
 		assert numTilingsMath(4) == 11;
-		assert numTilingsArray(699) == 939053561;
+		assert numTilingsMath(699) == 939053561;
+		System.out.println(numTilingsMath(699));
 	}
 
 	static int numTilings(int n) {
 		final Map<List<Integer>, Integer> t = new HashMap<>();
 		// trivial cases of the recursion.
-		t.put(Arrays.asList(0, 0), 1);
-		t.put(Arrays.asList(0, 1), 1);
+		t.put(Arrays.asList(1, 1), 1);
+		t.put(Arrays.asList(1, 2), 1);
 
-		t.put(Arrays.asList(1, 0), 1);
-		t.put(Arrays.asList(1, 1), 2);
-		t.put(Arrays.asList(1, 2), 2);
+		t.put(Arrays.asList(2, 1), 1);
+		t.put(Arrays.asList(2, 2), 2);
+		t.put(Arrays.asList(2, 3), 2);
 
-		for (int c1 = 2; c1 < n; c1++) {
+		for (int c1 = 3; c1 <= n; c1++) {
 			// case 1.
 			t.put(Arrays.asList(c1, c1 - 1),
 					(t.get(Arrays.asList(c1 - 2, c1 - 1)) + t.get(Arrays.asList(c1 - 2, c1 - 2))) % MOD);
@@ -44,22 +45,22 @@ class DominoAndTrominoTiling {
 			t.put(Arrays.asList(c1, c1 + 1),
 					(t.get(Arrays.asList(c1, c1 - 1)) + t.get(Arrays.asList(c1 - 1, c1 - 1))) % MOD);
 		}
-		return t.get(Arrays.asList(n - 1, n - 1));
+		return t.get(Arrays.asList(n, n));
 	}
 
 	static int numTilingsArray(int n) {
 		if (n == 1)
 			return 1;
 
-		final int[][] a = new int[n][3];
+		final int[][] a = new int[n + 1][3];
 		// trivial cases of the recursion.
-		a[0][1] = 1;
-		a[0][2] = 1;
-		a[1][0] = 1;
-		a[1][1] = 2;
-		a[1][2] = 2;
+		a[1][1] = 1;
+		a[1][2] = 1;
+		a[2][0] = 1;
+		a[2][1] = 2;
+		a[2][2] = 2;
 
-		for (int c1 = 2; c1 < n; c1++) {
+		for (int c1 = 3; c1 <= n; c1++) {
 			// case 1.
 			a[c1][0] = (a[c1 - 2][2] + a[c1 - 2][1]) % MOD;
 			// case 2.
@@ -69,7 +70,7 @@ class DominoAndTrominoTiling {
 			// case 3.
 			a[c1][2] = (a[c1][0] + a[c1 - 1][1]) % MOD;
 		}
-		return a[n - 1][1];
+		return a[n][1];
 	}
 
 	static int numTilingsMath(int n) {
