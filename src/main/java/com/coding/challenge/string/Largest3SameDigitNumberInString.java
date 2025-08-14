03e6c8460bade7e729e1785bbb83241296f678f4
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-08-15 07:55:18 +0800

    2264. Largest 3-Same-Digit Number in String.

---
+++ src/main/java/com/coding/challenge/string/Largest3SameDigitNumberInString.java
@@ -0,0 +1,35 @@
+package com.coding.challenge.string;
+
+class Largest3SameDigitNumberInString {
+	Largest3SameDigitNumberInString() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		assert "777".equals(largestGoodInteger("6777133339"));
+		assert "000".equals(largestGoodInteger("2300019"));
+		assert "".equals(largestGoodInteger("42352338"));
+	}
+
+	static String largestGoodInteger(String num) {
+		final int n = num.length();
+		char maxGood = '\0';
+		for (int i = 1, c = 1; i < n; i++) {
+			final char ch = num.charAt(i);
+			if (ch == num.charAt(i - 1))
+				c = c + 1;
+			else
+				c = 1;
+
+			if (c == 3) {
+				if (ch > maxGood)
+					maxGood = ch;
+			}
+		}
+		if (maxGood == '\0')
+			return "";
+
+		final char[] ans = { maxGood, maxGood, maxGood };
+		return new String(ans);
+	}
+}
