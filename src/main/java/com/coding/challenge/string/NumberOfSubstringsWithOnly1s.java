e12f5efdb6e395e0e00dc66abde96b176e6cb18c
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-11-16 10:25:35 +0800

    1513. Number of Substrings With Only 1s.

---
+++ src/main/java/com/coding/challenge/string/NumberOfSubstringsWithOnly1s.java
@@ -0,0 +1,27 @@
+package com.coding.challenge.string;
+
+class NumberOfSubstringsWithOnly1s {
+	private static final int MOD = 1000000007;
+
+	NumberOfSubstringsWithOnly1s() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		assert numSub("0110111") == 9;
+		assert numSub("101") == 2;
+		assert numSub("111111") == 21;
+	}
+
+	static int numSub(String s) {
+		final int n = s.length();
+		int c = 0;
+		for (int i = 0, lastZeroIdx = -1; i < n; i++) {
+			if (s.charAt(i) == '0')
+				lastZeroIdx = i;
+
+			c = (c + (i - lastZeroIdx)) % MOD;
+		}
+		return c;
+	}
+}
