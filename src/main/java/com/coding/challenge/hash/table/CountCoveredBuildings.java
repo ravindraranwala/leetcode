f406df2f9c666c1ba3fb23ec9c9d739b93a431ec
Ravindra Ranwala <ravindraranwala@gmail.com>
2025-12-13 10:19:07 +0800

    3531. Count Covered Buildings.

--- src/main/java/com/coding/challenge/hash/table/CountCoveredBuildings.java
+++ src/main/java/com/coding/challenge/hash/table/CountCoveredBuildings.java
@@ -12,11 +12,11 @@ class CountCoveredBuildings {
 		final int[][] buildings1 = { { 1, 2 }, { 2, 2 }, { 3, 2 }, { 2, 1 }, { 2, 3 } };
 		assert countCoveredBuildings(3, buildings1) == 1;
 
-		final int[][] b2 = { { 1, 1 }, { 1, 2 }, { 2, 1 }, { 2, 2 } };
-		assert countCoveredBuildings(3, b2) == 0;
+		final int[][] buildings2 = { { 1, 1 }, { 1, 2 }, { 2, 1 }, { 2, 2 } };
+		assert countCoveredBuildings(3, buildings2) == 0;
 
-		final int[][] b3 = { { 1, 3 }, { 3, 2 }, { 3, 3 }, { 3, 5 }, { 5, 3 } };
-		assert countCoveredBuildings(5, b3) == 1;
+		final int[][] buildings3 = { { 1, 3 }, { 3, 2 }, { 3, 3 }, { 3, 5 }, { 5, 3 } };
+		assert countCoveredBuildings(5, buildings3) == 1;
 	}
 
 	static int countCoveredBuildings(int n, int[][] buildings) {
