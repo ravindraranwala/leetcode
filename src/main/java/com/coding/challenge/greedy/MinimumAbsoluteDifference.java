ff2f8dd41ef0e00edc123755e5f11725cbc5c89e
Ravindra Ranwala <ravindraranwala@gmail.com>
2026-01-27 07:39:24 +0800

    1200. Minimum Absolute Difference.

--- src/main/java/com/coding/challenge/greedy/MinimumAbsoluteDifference.java
+++ src/main/java/com/coding/challenge/greedy/MinimumAbsoluteDifference.java
@@ -27,11 +27,11 @@ class MinimumAbsoluteDifference {
 		for (int i = 1; i < n; i++)
 			minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
 
-		final List<List<Integer>> ans = new ArrayList<>();
+		final List<List<Integer>> l = new ArrayList<>();
 		for (int i = 1; i < n; i++)
 			if (arr[i] - arr[i - 1] == minDiff)
-				ans.add(Arrays.asList(arr[i - 1], arr[i]));
+				l.add(Arrays.asList(arr[i - 1], arr[i]));
 
-		return ans;
+		return l;
 	}
 }
