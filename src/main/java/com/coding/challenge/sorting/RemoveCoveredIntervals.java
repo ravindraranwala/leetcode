662bd7450cfaa469e812b7635d4a8ba8e03e13b7
Ravindra Ranwala <ravindraranwala@gmail.com>
2026-07-07 07:52:07 +0800

    1288. Remove Covered Intervals.

--- src/main/java/com/coding/challenge/sorting/RemoveCoveredIntervals.java
+++ src/main/java/com/coding/challenge/sorting/RemoveCoveredIntervals.java
@@ -14,31 +14,29 @@ class RemoveCoveredIntervals {
 
 		final int[][] intervals2 = { { 1, 4 }, { 2, 3 } };
 		assert removeCoveredIntervals(intervals2) == 1;
-
-		final int[][] intervals3 = { { 1, 2 }, { 1, 4 }, { 3, 4 } };
-		assert removeCoveredIntervals(intervals3) == 1;
 	}
 
 	static int removeCoveredIntervals(int[][] intervals) {
-		final int n = intervals.length;
-		/*
-		 * sort intervals using start time, breaking ties using the reverse order of
-		 * finishing time.
-		 */
 		final Comparator<int[]> cmp = new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
-				final int comparisonOutcome = Integer.compare(o1[0], o2[0]);
-				return comparisonOutcome == 0 ? Integer.compare(o2[1], o1[1]) : comparisonOutcome;
+				final int ans = Integer.compare(o1[0], o2[0]);
+				if (ans == 0)
+					return Integer.compare(o2[1], o1[1]);
+
+				return ans;
 			}
 		};
+
 		Arrays.sort(intervals, cmp);
-		int remainingIntervals = n;
-		for (int i = 0, f = 0; i < n; i++) {
-			if (intervals[i][1] <= f)
-				remainingIntervals = remainingIntervals - 1;
-			f = Math.max(f, intervals[i][1]);
+		int maxFin = 0;
+		int remCnt = 0;
+		for (int[] interval : intervals) {
+			if (maxFin >= interval[1])
+				remCnt = remCnt + 1;
+			else
+				maxFin = interval[1];
 		}
-		return remainingIntervals;
+		return intervals.length - remCnt;
 	}
 }
