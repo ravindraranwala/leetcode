9d792108b5474aab9cf5d1d292de280b9be1a9f5
Ravindra Ranwala <ravindraranwala@gmail.com>
2026-07-17 07:53:18 +0800

    1291. Sequential Digits.

---
+++ src/main/java/com/coding/challenge/math/SequentialDigits.java
@@ -0,0 +1,46 @@
+package com.coding.challenge.math;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class SequentialDigits {
+	private static final int MAX_DIGIT = 9;
+
+	SequentialDigits() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final List<Integer> l1 = Arrays.asList(123, 23);
+		assert l1.equals(sequentialDigits(100, 300));
+
+		final List<Integer> l2 = Arrays.asList(1234, 2345, 3456, 4567, 5678, 6789, 12345);
+		assert l2.equals(sequentialDigits(1000, 13000));
+	}
+
+	static List<Integer> sequentialDigits(int low, int high) {
+		final List<Integer> ans = new ArrayList<>();
+		final int[] vals = new int[10];
+		for (int val = 1; val <= MAX_DIGIT; val++) {
+			vals[val] = val;
+			if (val >= low && val <= high)
+				ans.add(val);
+		}
+
+		for (int len = 2; len <= MAX_DIGIT; len++) {
+			for (int startingDigit = 1; startingDigit <= MAX_DIGIT; startingDigit++) {
+				int prevVal = vals[startingDigit];
+				final int lastDigit = prevVal % 10;
+				if (lastDigit < MAX_DIGIT) {
+					final int currVal = prevVal * 10 + lastDigit + 1;
+					vals[startingDigit] = currVal;
+					if (currVal >= low && currVal <= high)
+						ans.add(currVal);
+				}
+			}
+		}
+
+		return ans;
+	}
+}
