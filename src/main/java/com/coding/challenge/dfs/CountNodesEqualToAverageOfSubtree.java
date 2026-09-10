4061cc2290ba25f40a1952ea8bb1f1b1bb8c883b
Ravindra Ranwala <ravindraranwala@gmail.com>
2026-09-10 19:34:31 +0800

    2265. Count Nodes Equal to Average of Subtree.

--- src/main/java/com/coding/challenge/dfs/CountNodesEqualToAverageOfSubtree.java
+++ src/main/java/com/coding/challenge/dfs/CountNodesEqualToAverageOfSubtree.java
@@ -3,34 +3,35 @@ package com.coding.challenge.dfs;
 import com.coding.challenge.bst.TreeNode;
 
 class CountNodesEqualToAverageOfSubtree {
-	private int c = 0;
-
 	CountNodesEqualToAverageOfSubtree() {
-		
+		throw new AssertionError();
 	}
 
 	public static void main(String[] args) {
 		final TreeNode root1 = new TreeNode(4, new TreeNode(8, new TreeNode(0), new TreeNode(1)),
 				new TreeNode(5, null, new TreeNode(6)));
-		assert new CountNodesEqualToAverageOfSubtree().averageOfSubtree(root1) == 5;
+		assert averageOfSubtree(root1) == 5;
 
-		assert new CountNodesEqualToAverageOfSubtree().averageOfSubtree(new TreeNode(1)) == 1;
+		assert averageOfSubtree(new TreeNode(1)) == 1;
 	}
 
-	int averageOfSubtree(TreeNode root) {
-		postOrder(root);
-		return c;
+	static int averageOfSubtree(TreeNode root) {
+		final int[] ans = new int[1];
+		traverse(root, ans);
+		return ans[0];
 	}
 
-	private int[] postOrder(TreeNode root) {
+	private static int[] traverse(TreeNode root, int[] ans) {
 		if (root == null)
-			return new int[2];
-		final int[] l = postOrder(root.left);
-		final int[] r = postOrder(root.right);
-		final int sum = l[0] + r[0] + root.val;
-		final int count = l[1] + r[1] + 1;
-		if (sum / count == root.val)
-			c = c + 1;
-		return new int[] { sum, count };
+			return new int[] { 0, 0 };
+
+		final int[] left = traverse(root.left, ans);
+		final int[] right = traverse(root.right, ans);
+		final int sum = left[0] + right[0] + root.val;
+		final int cnt = left[1] + right[1] + 1;
+		if (root.val == sum / cnt)
+			ans[0] = ans[0] + 1;
+
+		return new int[] { sum, cnt };
 	}
 }
